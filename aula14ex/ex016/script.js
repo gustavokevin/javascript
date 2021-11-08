25a7731a193c983ae23ecb8c2f064c5e2fe0b4a7
Gustavo Kevin <92980634+gustavokevin@users.noreply.github.com>
2021-11-08 17:17:21 -0300

    Update script.js

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -15,6 +15,6 @@ function contar(){
         for (let c = 1; c <= f; c += p){
             res.innerHTML += `${c} \u{1F449} `
         }
-        res.innerHTML += `\u{1F3C1}`
+        res.innerHTML += `\u{1F3C1}` 
     }
 }
